c9eb8a9188e2dc9f139eac24f6b2b7cbfa8eab63
Firat-Gulec <firat.gulec@hotmail.com>
2022-10-15 15:42:26 +0300

    added some functions

---
+++ lib/core/Init/navigation/navigation_route.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+
+
+import '../../../features/login/view/login_view.dart';
+import '../../../features/login/view/signup_view.dart';
+import '../../../features/login/view/splash_screen.dart';
+import '../../../features/login/view/welcome_view.dart';
+import '../../../features/profile/view/profile_view.dart';
+import 'navigation_constants.dart';
+
+
+class NavigationRoute {
+  static final NavigationRoute _instance = NavigationRoute._init();
+  static NavigationRoute get instance => _instance;
+
+  NavigationRoute._init();
+
+  Route<dynamic> generateRoute(RouteSettings args) {
+    switch (args.name) {
+      case NavigationConstants.DEFAULT:
+        return normalNavigate(
+            // const SplashScreen(title: 'Splash Screen'), NavigationConstants.DEFAULT);
+            //SplashScreen(title: 'RXS'),
+            SplashScreen(title: 'RXS'),
+            NavigationConstants.DEFAULT);
+            
+      case NavigationConstants.LOGIN:
+        return normalNavigate(LoginView(), NavigationConstants.LOGIN);
+
+      case NavigationConstants.PROFILE_VIEW:
+        return normalNavigate(
+            const ProfileView(), NavigationConstants.PROFILE_VIEW);
+
+            case NavigationConstants.PROFILE_SIGNUP:
+        return normalNavigate(
+            const RegisterForm(), NavigationConstants.PROFILE_SIGNUP);
+       
+      case NavigationConstants.ON_BOARD:
+        return normalNavigate(
+            const WelcomeView(), NavigationConstants.ON_BOARD);
+ /*
+      case NavigationConstants.QUESTIONNAIRE:
+        return normalNavigate(
+            const WelcomeSurveyView(), NavigationConstants.QUESTIONNAIRE);
+
+      case NavigationConstants.HOME_VIEW:
+        return normalNavigate(const HomeView(), NavigationConstants.HOME_VIEW);
+
+      case NavigationConstants.CALENDAR:
+        return normalNavigate(
+            const CalendarView(), NavigationConstants.CALENDAR);
+
+      case NavigationConstants.ADDTASKVIEW:
+        return normalNavigate(
+          AddTaskView(model: args.arguments as String),
+          NavigationConstants.ADDTASKVIEW,
+        );*/
+
+      default:
+        return MaterialPageRoute(
+          builder: (context) => const WelcomeView(),
+        );
+    }
+  }
+
+  MaterialPageRoute normalNavigate(Widget widget, String pageName) {
+    return MaterialPageRoute(
+        builder: (context) => widget,
+        //??????
+        settings: RouteSettings(name: pageName));
+  }
+}
---
+++ lib/features/login/service/login_service.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../core/Init/navigation/navigation_service.dart';
+
+
+NavigationService navigation = NavigationService.instance;
+
+class LoginService {
+  Future<void> setLoginUser(String name, String password) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('username', name);
+    prefs.setString('password', password);
+  }
+  
+}
